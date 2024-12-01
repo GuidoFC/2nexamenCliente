4123b04939bf8b1e95cda6eafc2ea08ba307dbc1
GuidoFC <guidofigueroa96@gmail.com>
2024-12-01 22:37:29 +0100

    He borrado todo. Aqui hare el examen y esta es mi plantilla

--- Aeropuerto/Service/ServiceAeroport.js
+++ Aeropuerto/Service/ServiceAeroport.js
@@ -1,39 +1,13 @@
 import {Aeropuerto} from "../model/Aeropuerto.js";
 
 export class ServiceAeroport{
-    #url = "https://theteacher.codiblau.com/public/exercicis/airports";
+    #url = "????";
+
+
     async getAllAeroport(){
-        const listaAeropuerto = [];
-        const respuesta = await fetch(this.#url)
-        const data =  await respuesta.json();
-
-        data.map(r => {
-            const codi = r.code
-            const nom = r.name
-            const lati = r.lat
-            const long = r.lon
-            const ciutat = r.city
-            listaAeropuerto.push(new Aeropuerto(codi, nom,lati,long, ciutat))
-
-        })
-        return listaAeropuerto;
+        console.log("Funciona!!!!")
     }
 
-    searchAeroport(inputIntroducido, listaAeropuerto){
-        const inputMayusc = inputIntroducido.toUpperCase();
-
-        console.log(inputMayusc);
-
-        // si quieres limiar el array
-        // listaAeropuerto.slice(1,3).map()
-        return  listaAeropuerto.filter(r =>{
-            const codi = r.codi.toUpperCase().includes(inputMayusc)
-            const nom = r.nom.toUpperCase().includes(inputMayusc)
-            const ciutat = r.ciutat.toUpperCase().includes(inputMayusc)
-            return  nom || codi || ciutat
-        })
-
-    }
 
 
 }
--- Aeropuerto/View/viewAeropuerto.js
+++ Aeropuerto/View/viewAeropuerto.js
@@ -1,65 +1,14 @@
 import {ServiceAeroport} from "../Service/ServiceAeroport.js";
 
-function createSearch() {
-    const getApp = document.querySelector("#App")
-
-    const newInput = document.createElement("input")
-    const newlabel = document.createElement("label")
-    newInput.setAttribute("type", "search");
-    newInput.id = "input";
-
-    newlabel.textContent = "Buscar Aerropuerto"
-
-
-    getApp.appendChild(newlabel);
-    getApp.appendChild(newInput);
-
-}
-
-createSearch();
-
-function crearDivMostrar() {
-    const div = document.createElement("div");
-    div.id = "listAeropuerto"
-    div.textContent ="Lista de aeropuertos Encontrados"
-    const getEle = document.querySelector("#App");
-    getEle.appendChild(div)
-}
-
-crearDivMostrar()
-
-function divMuestraRespuestas(aeropEncontrados) {
-    const getDivLista = document.querySelector("#listAeropuerto");
-
-    getDivLista.textContent = "";
-
-
-    for (const aeropueto of aeropEncontrados) {
-        const p = document.createElement("p");
-        p.textContent = aeropueto.nom + " latitud " + aeropueto.latitud + " longitud " + aeropueto.longitud;
-        getDivLista.appendChild(p)
-    }
-}
-
-
 
 (async () => {
 
     const service = new ServiceAeroport
-    const listaAeropuerto = await service.getAllAeroport();
+    // const listaAeropuerto = await service.getAllAeroport();
 
     // piensa que listaAeropuerto es un array
 
-    const getInput = document.querySelector("#input")
-
-    getInput.addEventListener("input", function () {
-        const valorIntroducido = this.value;
-
-        const aeropEncontrados = service.searchAeroport(valorIntroducido, listaAeropuerto)
-
-        divMuestraRespuestas(aeropEncontrados)
-
-    })
+    service.getAllAeroport();
 
 
 })();
